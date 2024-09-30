d3dad3cff3235a59b225a32a4cdaabd828a86645
Jadielson182 <Jadielson182@users.noreply.github.com>
2024-09-29 22:03:44 -0300

    att

--- codigos107_site/codigos107.py
+++ codigos107_site/codigos107.py
@@ -4,3 +4,9 @@ import requests
 # https:// -> 443
 # caso nao informa a porta abaixo o padrao seria essers acima para http e https
 url = 'http://localhost:8000/'
+response = requests.get(url)
+print(response.status_code)
+# print(response.headers)
+# print(response.content)~
+# print(response.json())
+print(response.text)
